9c5b47b75e813a474d2842f303620a0ed4c6ef8d
Jon Campbell <joncamp@microsoft.com>
2018-08-09 16:15:52 -0700

    General Updates
    Reduce buttons from 12 to 4
    Change from internal pullup to external pullup resistor setting
    Change to a timer based loop instead of delay

--- Switch37.ino
+++ Switch37.ino
@@ -9,53 +9,67 @@
 //
 // By Jon Campbell (2018-07-30)
 // Based on an example by Matthew Heironimus (2016-11-24)
-// 
+//
 //--------------------------------------------------------------------
 
 #include <Joystick.h>
 
-#define BUTTONCOUNT                         12
-#define AXISCOUNT                           6
+#define BUTTONCOUNT 4
 
 Joystick_ Gamepad(0x03, JOYSTICK_TYPE_GAMEPAD,
-   BUTTONCOUNT,     0,   // Button Count, Hat Switch Count
-  false, false, false,   // X and Y, but no Z Axis
-  false, false, false,   // No Rx, Ry, or Rz
-  false, false,          // No rudder or throttle
-  false, false, false);  // No accelerator, brake, or steering
+                  BUTTONCOUNT, 0,       // Button Count, Hat Switch Count
+                  false, false, false,  // X and Y, but no Z Axis
+                  false, false, false,  // No Rx, Ry, or Rz
+                  false, false,         // No rudder or throttle
+                  false, false, false); // No accelerator, brake, or steering
 
 // Last state of the buttons
-int buttonPins[BUTTONCOUNT]       = {  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13};
-int lastButtonState[BUTTONCOUNT]  = {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+int buttonPins[BUTTONCOUNT] = {4, 5, 6, 7};
+int lastButtonState[BUTTONCOUNT] = {0, 0, 0, 0};
 
-void setup() {
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 10; // interval at which to poll (milliseconds)
+long previousMillis = 0;
+
+void setup()
+{
   // Initialize Button Pins
   for (int index = 0; index < BUTTONCOUNT; ++index)
   {
     // Note: If using external pullup resistors, switch this to be INPUT
-    pinMode(buttonPins[index], INPUT_PULLUP);
+    pinMode(buttonPins[index], INPUT);
   }
 
   // Initialize Joystick Library
   Gamepad.begin();
 }
 
-void loop() {
-  // Read button pin values
-  for (int index = 0; index < BUTTONCOUNT; index++)
-  {
-    // Read the button state
-    int currentButtonState = !digitalRead(buttonPins[index]);
+void loop()
+{
+  // check to see if it's time to blink the LED; that is, if the
+  // difference between the current time and last time you blinked
+  // the LED is bigger than the interval at which you want to
+  // blink the LED.
+  unsigned long currentMillis = millis();
 
-    // Only send the state if the state has changed
-    if (currentButtonState != lastButtonState[index])
+  if (currentMillis - previousMillis > interval)
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // Read button pin values
+    for (int index = 0; index < BUTTONCOUNT; index++)
     {
-      Gamepad.setButton(index, currentButtonState);
-      lastButtonState[index] = currentButtonState;
+      // Read the button state
+      int currentButtonState = !digitalRead(buttonPins[index]);
+
+      // Only send the state if the state has changed
+      if (currentButtonState != lastButtonState[index])
+      {
+        Gamepad.setButton(index, currentButtonState);
+        lastButtonState[index] = currentButtonState;
+      }
     }
   }
-
-  // TODO Switch this to using a timing loop rather than delay
-  delay(10);
 }
-
